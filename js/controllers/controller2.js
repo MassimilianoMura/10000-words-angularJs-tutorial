bb44a59ec4b0019c171cc24a2d65fb29e93df5dc
MassimilianoMura <massimilianomura1983@gmail.com>
2015-10-20 16:05:29 +0100

    ads a new controller to try the controllerAS syntax

---
+++ js/controllers/controller2.js
@@ -0,0 +1,18 @@
+app.controller('mySecondCtrl', function() {
+
+  this.items = [{
+    name: 'Scuba Diving Kit 2',
+    id: 7297510
+  },{
+    name: 'Snorkel 2',
+    id: 0278916
+  },{
+    name: 'Wet Suit 2',
+    id: 2389017
+  },{
+    name: 'Beach Towel 2',
+    id: 1000983
+  }];
+
+
+});
