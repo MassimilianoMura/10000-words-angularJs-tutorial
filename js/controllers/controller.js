bb14fdf2e2a82a6504abfa835414f12f38b4a9bd
MassimilianoMura <massimilianomura1983@gmail.com>
2015-10-20 15:40:13 +0100

    ads a ng-repeat to dislay a loist of items

--- js/controllers/controller.js
+++ js/controllers/controller.js
@@ -2,4 +2,19 @@ app.controller('myCtrl', ['$scope', function($scope) {
 
   $scope.greeting = 'Hola!';
 
+  $scope.items = [{
+    name: 'Scuba Diving Kit',
+    id: 7297510
+  },{
+    name: 'Snorkel',
+    id: 0278916
+  },{
+    name: 'Wet Suit',
+    id: 2389017
+  },{
+    name: 'Beach Towel',
+    id: 1000983
+  }];
+
+
 }]);
